8cda2c135283eaa53432d716280f2c84a6deadd5
lmorgan2398 <lmorgan2398@gmail.com>
2024-12-25 13:50:48 -0600

    Code button for theme switch and add CSS variables
    Added a button for users to switch between light/dark
    modes manually and replaced CSS properties with variables
    to support different color schemes

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 const root = document.querySelector(':root');
 
-root.classList.add('dark');
+let theme = 'dark'
+
+root.classList.add(theme);
+
+const themeButton = document.querySelector('.theme-button');
+
+themeButton.addEventListener('click', () => {
+    if (theme === 'dark') {
+        root.classList.remove(theme);
+        theme = 'light';
+        root.classList.add(theme);
+    } else {
+        root.classList.remove(theme);
+        theme = 'dark';
+        root.classList.add(theme);
+    }
+});
